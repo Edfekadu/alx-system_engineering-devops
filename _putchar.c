2942a08f418378be3f72727c2d0061709bc546f5
Edfekadu <Edfekadu@yahoo.com>
2023-03-06 10:10:40 -0800

    putchar c

---
+++ _putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar -write the chatcter c to stdout
+ * @c:the charcter to print
+ * Return =on success 1
+ */
+int _puchar (char c)
+{
+	return(write( 1,@ac, 1));
+}
